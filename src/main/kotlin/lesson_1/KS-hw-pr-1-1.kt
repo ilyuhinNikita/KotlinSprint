2a97461c0f02c9972be8d6ad84647245c709211d
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-04 23:00:37 +0300

    KS-homework-1-1 do orders and message

---
+++ src/main/kotlin/lesson_1/KS-hw-pr-1-1.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+    fun main(){
+
+       val numberOfOrders: Byte = 75
+       val thanksMessage: String = "Thank you for purchase!\nSee you soon!"
+
+//       println(numberOfOrders)
+       println(thanksMessage)
+
+    }
+
+
+
+
