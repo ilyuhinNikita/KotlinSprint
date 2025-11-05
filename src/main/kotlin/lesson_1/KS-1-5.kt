1551b765c37ba410c37946989c801fa842d07b09
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-05 23:03:12 +0300

    KS-1-5 final print time

--- src/main/kotlin/lesson_1/KS-1-5.kt
+++ src/main/kotlin/lesson_1/KS-1-5.kt
@@ -7,12 +7,6 @@ fun main() {
     val hours = (minutes / 60)
     val leftMinutes = (minutes % 60)
     val leftSeconds = (seconds % 60)
-
-//    println(minutes)
-//    println(hours)
-//    println(leftMinutes)
-//    println(leftSeconds)
-
     val time = String.format("%02d:%02d:%02d;", hours, leftMinutes, leftSeconds)
     println(time)
 
