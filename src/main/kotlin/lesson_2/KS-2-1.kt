8ffa15da3cfdd7ad467d83484adf71ae5cc2f51b
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-05 23:45:14 +0300

    KS-2-1 average score

--- src/main/kotlin/lesson_2/KS-2-1.kt
+++ src/main/kotlin/lesson_2/KS-2-1.kt
@@ -1,2 +1,11 @@
 package org.example.lesson_2
 
+fun main() {
+
+    val floatNum1: Float = 3f
+    val floatNum2: Float = 4f
+    val floatNum3: Float = 5f
+
+    println((floatNum1 + floatNum2 + floatNum1 + floatNum3) / floatNum2)
+
+}
