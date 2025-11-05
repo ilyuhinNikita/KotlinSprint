e83d53ec366eb401a648efa9e251cb307da17678
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-05 22:58:43 +0300

    KS-1-5 first prin

--- src/main/kotlin/lesson_1/KS-1-5.kt
+++ src/main/kotlin/lesson_1/KS-1-5.kt
@@ -2,6 +2,18 @@ package org.example.lesson_1
 
 fun main() {
 
+    val seconds = 6480
+    val minutes = (seconds / 60)
+    val hours = (minutes / 60)
+    val leftMinutes = (minutes % 60)
+    val leftSeconds = (seconds % 60)
 
+//    println(minutes)
+//    println(hours)
+//    println(leftMinutes)
+//    println(leftSeconds)
+
+    val time = String.format("%02d:%02d:%02d;", hours, leftMinutes, leftSeconds)
+    println(time)
 
 }
