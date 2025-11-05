60e004bdca5127b222b6b5b548d2eb8984792471
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-05 22:17:54 +0300

    KS-1-4 information

--- src/main/kotlin/lesson_1/KS-1-4.kt
+++ src/main/kotlin/lesson_1/KS-1-4.kt
@@ -1,2 +1,14 @@
 package org.example.lesson_1
 
+fun main() {
+
+    val lenght: Long = 4086860000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Float = 2.0547945205479453E-4f
+    val apogee: Int = 327000
+
+    println("$lenght\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
+
+}
