b4537296608a2655e67920d6b99c01ca0071f21b
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-06 19:39:17 +0300

    KS-1-5 second refactoring

--- src/main/kotlin/lesson_1/KS-1-5.kt
+++ src/main/kotlin/lesson_1/KS-1-5.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_1
 
-const val timeInterval = 60
+const val TIME_INTERVAL = 60
+
 fun main() {
 
     val seconds = 6480
 
-    val minutes = (seconds / timeInterval)
-    val hours = (minutes / timeInterval)
-    val leftMinutes = (minutes % timeInterval)
-    val leftSeconds = (seconds % timeInterval)
+    val minutes = seconds / TIME_INTERVAL
+    val hours = minutes / TIME_INTERVAL
+    val leftMinutes = minutes % TIME_INTERVAL
+    val leftSeconds = seconds % TIME_INTERVAL
 
     val time = "%02d:%02d:%02d;".format(hours, leftMinutes, leftSeconds)
     println(time)
