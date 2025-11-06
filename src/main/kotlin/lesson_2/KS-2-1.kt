0ffae18008ae2088724ae5f0b310bbd78cee03b1
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-06 20:22:25 +0300

    KS-2-1 refactoring

--- src/main/kotlin/lesson_2/KS-2-1.kt
+++ src/main/kotlin/lesson_2/KS-2-1.kt
@@ -2,10 +2,14 @@ package org.example.lesson_2
 
 fun main() {
 
-    val floatNum1: Float = 3f
-    val floatNum2: Float = 4f
-    val floatNum3: Float = 5f
+    val students = 4.0
+    val firstStudent = 3
+    val secondStudent = 4
+    val thirdStudent = 3
+    val fourthStudent = 5
 
-    println((floatNum1 + floatNum2 + floatNum1 + floatNum3) / floatNum2)
+    val average = (firstStudent + secondStudent + thirdStudent + fourthStudent) / students
+
+    println(average)
 
 }
