34f8f6d72aacd10ce9a90c0b3d5b49de3d5fcfb3
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-07 13:47:17 +0300

    KS-1-4 refactoring second save

--- src/main/kotlin/lesson_1/KS-1-4.kt
+++ src/main/kotlin/lesson_1/KS-1-4.kt
@@ -6,8 +6,8 @@ fun main() {
     val age: Byte = 27
     val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
+    val partOfYear: Double = 2.0547945205479453E-4
 
     println("$lenght\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
 
