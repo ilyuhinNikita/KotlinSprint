f7f8689cf0675d734b2fcdd565d6fcbede9338a4
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-08 01:42:10 +0300

    KS-2-4 final

--- src/main/kotlin/lesson_2/KS-2-4.kt
+++ src/main/kotlin/lesson_2/KS-2-4.kt
@@ -1,2 +1,19 @@
 package org.example.lesson_2
 
+const val DIVIDER = 100
+
+fun main() {
+
+    val crystal = "Crystal"
+    val iron = "Iron"
+    val crystalHave = 7
+    val ironHave = 11
+    val buffPercentage = 20
+
+    val crystalBuff = crystalHave * buffPercentage / DIVIDER
+    val ironBuff = ironHave * buffPercentage / DIVIDER
+
+    println("$crystal:$crystalBuff")
+    println("$iron:$ironBuff")
+
+}
