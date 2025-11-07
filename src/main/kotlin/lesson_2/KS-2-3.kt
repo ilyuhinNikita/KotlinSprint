a58743368169af3fd05137e74f7b2acfe5ebab3d
ilyuhin <nikitailyuhin1996@gmail.com>
2025-11-08 00:04:39 +0300

    KS-2-3 half result

--- src/main/kotlin/lesson_2/KS-2-3.kt
+++ src/main/kotlin/lesson_2/KS-2-3.kt
@@ -1,2 +1,27 @@
 package org.example.lesson_2
 
+    const val TIME_CONST = 60
+
+fun main() {
+
+    val departureHour = 9
+    val departureMinutes = 39
+    val travelMinutes = 457
+
+    val travelHour = travelMinutes / TIME_CONST
+    var arrivalHour = departureHour + travelMinutes / TIME_CONST
+    val leftMinutes = travelMinutes - travelHour * TIME_CONST
+    val allMinutes = departureMinutes + leftMinutes
+    val arrivalMinutes = (departureMinutes + leftMinutes) % TIME_CONST
+
+    if (allMinutes >= TIME_CONST) {
+        println(arrivalHour + 1)
+    }
+
+
+    println(travelHour)
+    println(arrivalHour)
+    println(leftMinutes)
+    println(allMinutes)
+    println(arrivalMinutes)
+}
